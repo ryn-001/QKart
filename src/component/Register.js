bd504a327817fdda54d40f0cc25d927751344498
Aryan Khaire <aryan.khaire001@gmail.com>
2025-09-26 22:50:18 +0530

    Register done

--- src/component/Register.js
+++ src/component/Register.js
@@ -4,6 +4,8 @@ import {useSnackbar} from "nostick";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import {config} from "../App"
+import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Register = () => {
     const {enqueueSnackBar} = useSnackbar();
@@ -107,9 +109,73 @@ const Register = () => {
     }
 
     return(
-        <>
-        
-        </>
+        <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <Header hasHiddenAuthButtons/>
+
+            <Box className="content">
+                <Stack spacing={2} className="form">
+                    <h2 className="title">Register</h2>
+                    <TextField
+                        id="username"
+                        label="username"
+                        value={formData.username}
+                        onChange={change}
+                        variant="outlined"
+                        title="Username"
+                        name="username"
+                        placeholder="Enter your username"
+                        fullWidth
+                    />
+                    
+                    <TextField 
+                        id="password"
+                        label="password"
+                        value={formData.password}
+                        onChange={change}
+                        variant="outlined"
+                        name="password"
+                        type="password"
+                        helperText="Password must be atleast 6 characters length"
+                        fullWidth
+                        placeholder="Enter the password with minimum 6 characters"
+                    />
+                    
+                    <TextField 
+                        id="confirm-password"
+                        variant="outlined"
+                        value={formData.confirmPassword}
+                        onChange={change}
+                        label="Confirm Password"
+                        name="confirmPassword"
+                        type="password"
+                        fullWidth
+                    />
+
+                    {loading ? (
+                        <CircularProgress size={24}/>
+                    ) : (
+                        <Button
+                            className="button"
+                            variant="contained"
+                            onClick={() => register(formData)}
+                            disabled={loading}
+                        >Register Now</Button>
+                    )}
+                    <p className="secondary-action">
+                        Already have an account ?{" "}
+                        <Link to="/login">
+                            Login Here
+                        </Link>
+                    </p>
+                </Stack>
+            </Box>
+
+            <Footer/>
+        </Box>
     )
 }
 
